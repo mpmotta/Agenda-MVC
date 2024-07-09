ce9f5214eab5290b0c2e7669d156651a5bda0cdf
Márcio Motta <mpmotta@gmail.com>
2024-07-09 11:05:54 -0300

    Add files via upload

--- controller/pessoaController.php
+++ controller/pessoaController.php
@@ -15,14 +15,8 @@ class pessoaController {
         return $result;
     }
 
-    public function inserir() {
-        $pessoa = array(
-            'nome' => $_POST['nome'],
-            'fone' => $_POST['fone'],
-            'email' => $_POST['email']
-        );
-
-        if (strlen($pessoa['nome']) == 0 || strlen($pessoa['fone']) == 0 || strlen($pessoa['email']) == 0) {
+    public function inserir(Pessoa $pessoa) {
+        if (strlen($pessoa->getNome()) == 0 || strlen($pessoa->getFone()) == 0 || strlen($pessoa->getEmail()) == 0) {
             header("Location: ../view/cadastro.php?campoVazio");
         } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $pessoaModel = new Pessoa();
@@ -33,28 +27,19 @@ class pessoaController {
         }
     }
 
-    public function editar() {
-        $id = $_POST['meuid'];
-        $pessoa = array(
-            'avatar' => $_POST['meuavatar'],
-            'nome' => $_POST['nome'],
-            'fone' => $_POST['fone'],
-            'email' => $_POST['email']
-        );
-
-        if (strlen($pessoa['nome']) == 0 || strlen($pessoa['fone']) == 0 || strlen($pessoa['email']) == 0) {
-            header("Location: ../view/editar.php?id=$id&campoVazio");
+    public function editar(Pessoa $pessoa) {
+        if (strlen($pessoa->getNome()) == 0 || strlen($pessoa->getFone()) == 0 || strlen($pessoa->getEmail()) == 0) {
+            header("Location: ../view/editar.php?id={$pessoa->getId()}&campoVazio");
         } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $pessoaModel = new Pessoa();
-            $pessoaModel->editar($pessoa, $id);
+            $pessoaModel->editar($pessoa, $pessoa->getId());
             header("Location: ../view/agenda.php?edit=ok");
         } else {
             header("Location: ../view/agenda.php?erro=erro");
         }
     }
 
-    public function excluir() {
-        $id = $_GET['id'];
+    public function excluir($id) {
         $pessoaModel = new Pessoa();
         $pessoaModel->excluir($id);
         header("Location: ../view/agenda.php?delete=ok");
@@ -62,13 +47,23 @@ class pessoaController {
 
     public function handleRequest() {
         if (isset($_GET['action']) && $_GET['action'] == 'cadastrarPessoa') {
-            $this->inserir();
+            $pessoa = new Pessoa();
+            $pessoa->setNome($_POST['nome']);
+            $pessoa->setFone($_POST['fone']);
+            $pessoa->setEmail($_POST['email']);
+            $this->inserir($pessoa);
         }
         if (isset($_GET['action']) && $_GET['action'] == 'editarPessoa') {
-            $this->editar();
+            $pessoa = new Pessoa();
+            $pessoa->setId($_POST['meuid']);
+            $pessoa->setAvatar($_POST['meuavatar']);
+            $pessoa->setNome($_POST['nome']);
+            $pessoa->setFone($_POST['fone']);
+            $pessoa->setEmail($_POST['email']);
+            $this->editar($pessoa);
         }
         if (isset($_GET['action']) && $_GET['action'] == 'excluirPessoa') {
-            $this->excluir();
+            $this->excluir($_GET['id']);
         }
     }
 }
