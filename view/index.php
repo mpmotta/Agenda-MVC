c5fa2cff81c85df27ee8d506a4bb8c5e4cfbaacf
MARCIO PAIVA DA MOTTA <71364579049@senacrs.edu.br>
2026-05-22 10:22:10 -0300

    tarefa 504-101

--- view/index.php
+++ view/index.php
@@ -22,11 +22,15 @@
                 placeholder="Senha">
             </div> 
             <div class="m-3">   
-            <button type="submit" id="submit" class="btn btn-primary mb-3">
+            <button type="submit" id="submit" class="form-control btn btn-primary mb-3">
                     Logar
                 </button>
             </div>    
         </form>
+        <div class="m-3">
+        <a href="../controller/esqueciSenha.php"
+        class="form-control btn btn-warning mb-3">Esqueci a Senha</a>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <?php
